6dd5ce10f71d963c25157e77a2b74fc706ba2550
Per Jensen <perjmi@yahoo.com>
2024-10-11 07:06:37 +0200

    initial

---
+++ src/test/java/com/example/attraction/AttractionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.attraction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttractionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
